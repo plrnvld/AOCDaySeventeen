8a11d364e1f7abfb232df60d9268f7dd4cb0fcdf
plrnvld <paulrijneveld@gmail.com>
2022-01-03 23:11:04 +0000

    Part 2

--- index.ts
+++ index.ts
@@ -1,83 +1,53 @@
 // Example input
-
+/*
 const min_target_x = 20;
 const max_target_x = 30;
 const min_target_y = -10;
 const max_target_y = -5;
-
+*/
 
 // Problem input
-/*
+
 const min_target_x = 217;
 const max_target_x = 240;
 const min_target_y = -126;
 const max_target_y = -69;
-*/
-
-const epsilon = 0.0000000001;
 
 var solutions: [number, number][] = [];
 
 function add_solution(new_solution: [number, number]) {
-    let match = solutions.find(item => is_equal_to(item[0], new_solution[0]) && is_equal_to(item[1], new_solution[1]));
+    let match = solutions.find(item => item[0] === new_solution[0] && item[1] === new_solution[1]);
 
     if (!match) {
         solutions.push(new_solution);
     }
 }
 
-console.log(`Running`);
-
 for (var x = min_target_x; x <= max_target_x; x++) {
     let max_steps_for_pos = find_max_num_steps(x);
     console.log(`** Checking x=${x}, max steps = ${max_steps_for_pos}`);
+    
+    for (var y = min_target_y; y <= max_target_y; y++) {
+        
+        for (var num_steps = max_steps_for_pos; num_steps > 0; num_steps--) {
+            let vel_x = find_required_start_velocity(x, num_steps);
+            let vel_ys = find_required_start_height_super(y, num_steps, max_steps_for_pos);
 
-    if (is_equal_to(x, sum_step_sequence(max_steps_for_pos))) {
-        let size1 = solutions.length;
-        fire_wild(x, is_in_target);
-        let size2 = solutions.length;
-        console.log(`>> Firing wild for pos=${x} gave ${size2 - size1} solutions`);
-    } else {
-        for (var y = min_target_y; y <= max_target_y; y++) {
-            
-            for (var num_steps = max_steps_for_pos; num_steps > 0; num_steps--) {
-                let vel_x = find_required_start_velocity(x, num_steps);
-                let vel_y = find_required_start_height(y, num_steps);
-
+            for (let vel_y of vel_ys) {            
                 if (vel_x !== null && vel_y !== null) {
                     let new_solution: [number, number] = [vel_x!, vel_y!];
                     let hitcheck = fire(new_solution, is_in_target);
-                    console.log(`   Solution (${new_solution}) found for [${x},${y}] with ${num_steps} steps. Check: ${hitcheck}`);
-                    add_solution(new_solution);
+                    // console.log(`   Solution (${new_solution}) found for [${x},${y}] with ${num_steps} steps. Check: ${hitcheck}`);
+
+                    if (hitcheck)
+                        add_solution(new_solution);
                 }
-            }     
-        }
+            }
+        }     
     }
 }
-
-
-// 2239 too low
-// 2355 too high
-
-
-
-
 
 console.log(`Solutions length: ${solutions.length}`);
-
-
-function fire_wild(horizontal_step: number, target_check: TargetCheck) {
-    for (var i = -100; i <= 200; i++) {
-        var velocity:[number, number] = [horizontal_step, i];
-        var is_hit = fire(velocity, target_check);
-        if (is_hit)
-            console.log(`      Firing: (${velocity}) ==> ${is_hit ? "hit" : "miss"}`);
-
-        if (is_hit) {
-            add_solution(velocity);
-        }
-    }
-}
 
 function fire(velocity: [number, number], target_check: TargetCheck): boolean {
     let pos: [number, number] = [0, 0];
@@ -92,14 +62,6 @@ function fire(velocity: [number, number], target_check: TargetCheck): boolean {
     }
 
     return false;
-}
-
-function max_height(step: [number, number]): number {
-    let y = step[1];
-
-    return y <= epsilon
-        ? 0
-        : sum_step_sequence(y);
 }
 
 function sum_step_sequence(maxStep: number): number {
@@ -125,6 +87,26 @@ function find_required_start_height(end_height: number, num_steps: number): numb
         : null;
 }
 
+function find_required_start_height_super(end_height: number, num_steps: number, max_steps: number): number[] {
+    let vertical_speedup = sum_step_sequence(num_steps - 1);
+    
+    var results: number[] = [];
+        
+    let max_vertical_flight = num_steps === max_steps // Probe falls vertically at the end
+        ? 10000
+        : 0;
+
+    for (var vertical_flight = 0; vertical_flight <= max_vertical_flight; vertical_flight++) {
+        let start_height = (end_height + vertical_speedup + vertical_flight) / num_steps;
+    
+        if (is_whole(start_height)) {
+            results.push(start_height!);
+        }
+    }
+
+    return results;
+}
+
 function find_max_num_steps(pos: number): number {
     return Math.floor(find_required_step_decimal(pos));
 }
@@ -142,18 +124,7 @@ function find_required_step_decimal(pos: number): number {
 }
 
 function next_velocity(velocity: [number, number], iterations: number = 1): [number, number] {
-    var x = velocity[0];
-    var y = velocity[1];
-
-    var intermediate_x = x - iterations;
-
-    var next_x = is_smaller_than_or_equal_to(intermediate_x, 0)
-        ? 0
-        : intermediate_x;
-
-    var next_y = y - iterations;
-
-    return [next_x, next_y];
+    return [Math.max(velocity[0] - iterations, 0), velocity[1] - iterations];
 }
 
 type TargetCheck = (pos: [number, number]) => boolean;
@@ -166,34 +137,20 @@ function is_in_target(pos: [number, number]): boolean {
 }
 
 function is_in_column_of_target(pos: [number, number], column_to_hit: number) {
-    return is_in_target(pos) && is_equal_to(pos[0], column_to_hit);
+    return is_in_target(pos) && pos[0] === column_to_hit;
 }
 
 function hits_position(pos: [number, number], pos_to_hit: [number, number]) {
-    return is_equal_to(pos[0], pos_to_hit[0]) 
-        && is_equal_to(pos[1], pos_to_hit[1]);
+    return pos[0] === pos_to_hit[0] && pos[1] === pos_to_hit[1];
 }
 
 function passed_target(pos: [number, number]): boolean {
     let x = pos[0];
     let y = pos[1];
     
-    return is_larger_than_or_equal_to(x, max_target_x + 1) 
-        || is_smaller_than_or_equal_to(y, min_target_y - 1);
-}
-
-function is_smaller_than_or_equal_to(num: number, limit: number) : boolean {
-    return num < limit + epsilon;
-}
-
-function is_larger_than_or_equal_to(num: number, limit: number) : boolean {
-    return num > limit + epsilon;
+    return x > max_target_x || y < min_target_y;
 }
 
 function is_whole(num: number): boolean {
-    return Math.abs(num % 1) < epsilon;
-}
-
-function is_equal_to(num: number, target: number) {
-    return Math.abs(num - target) < epsilon;
+    return Math.abs(num % 1) === 0;
 }
